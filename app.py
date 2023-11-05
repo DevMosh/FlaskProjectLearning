deae63335d7473fd8b9f42b1ff6c90b2c89b52dd
DevMosh <49912219+DevMosh@users.noreply.github.com>
2023-11-05 13:02:25 +0300

    added news content and improving style, used jinja2

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 # WERKZEUG
 # JINJA2
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect
 import sqlite3
 
 app = Flask(__name__)  # создается объект приложения
@@ -21,13 +21,22 @@ app = Flask(__name__)  # создается объект приложения
 connection = sqlite3.connect('my_database.db')
 cursor = connection.cursor()
 cursor.execute('''
-        CREATE TABLE IF NOT EXISTS News (
+        CREATE TABLE IF NOT EXISTS Users (
         name TEXT NOT NULL UNIQUE,
         password TEXT NOT NULL
         )
         ''')
+cursor.execute('''
+        CREATE TABLE IF NOT EXISTS News (
+        name TEXT NOT NULL,
+        content TEXT NOT NULL
+        )
+        ''')
+cursor.close()
+connection.close()
 
 
+@app.route('/about')
 @app.route('/')
 @app.route('/home.html')
 def hello_world():  # put application's code here
@@ -96,30 +105,168 @@ def registration():
 
 
 @app.route("/all-news")
-def all_news_func():
-    return 'тут будут все новости от всех авторов этого сайта'
+@app.route("/all-news/<string:login>")
+def all_news_func(login=None):
+    connection = sqlite3.connect('my_database.db')
+    cursor = connection.cursor()
+    print(cursor.execute("SELECT * FROM users WHERE name=?", (login, )).fetchone())
+
+    news = []
+    if login == None:
+        text_author = ''
+        news = cursor.execute("SELECT * FROM news").fetchall()
+    else:
+        news = cursor.execute("SELECT * FROM news WHERE name=?", (login, )).fetchall()
+        text_author = f'FROM THE AUTHOR {login}'
+    news_block = ""
+
+    print(news)
+
+    if news == []:
+        news_block += f'<h1>У автора нет новостей</h1>'
+    else:
+        for i in news:
+            news_block += f'<div class="news-block">{i[1]}</div>'
+
+    html = f"""<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="{ url_for('static', filename='css/home.css') }">
+</head>
+<body>
+    <header>
+    <div>
+       <div class="logo">
+        <a href="home.html">
+          <span class="use">NEWS</span>-<span class="web">NEWS</span>.ru
+        </a>
+       </div>
+
+
+       <div class="top-menu">
+         <ul>
+             <li><a class="clickMenu" href="/all-news">Все новости</a></li>
+             <li><a href="/all-author">Все авторы</a></li>
+             <li><a href="/add-news">Добавить новость</a></li>
+         </ul>
+       </div>
+
+
+       <div class="block-top-auth">
+         <p><a href="/login">Вход</a></p>
+         <p><a href="/registration">Регистрация</a></p>
+       </div>
+
+    </header>
+
+    <br>
+    <h1 style="text-align: center;">ALL NEWS {text_author}</h1>
+    <br>
+    <hr>
+    <div>
+    
+    {news_block}
+
+</body>
+</html>"""
+
+
+    return html
 
 @app.route("/all-author")
 def all_author_func():
-    return 'тут будут все зарегистрированные авторы этого сайта'
+    connection = sqlite3.connect('my_database.db')
+    cursor = connection.cursor()
+    news = cursor.execute("SELECT * FROM users").fetchall()
+    connection.commit()
+    cursor.close()
+    connection.close()
+    print(news)
+    news_block = ""
+    for i in news:
+        news_block += f'<a href="/all-news/{i[0]}"><div class="authors"><h4>Автор: {i[0]}\n</h4></div></a>'
+    html = f"""<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Title</title>
+        <link rel="stylesheet" href="{url_for('static', filename='css/home.css')}">
+    </head>
+    <body>
+        <header>
+        <div>
+           <div class="logo">
+            <a href="home.html">
+              <span class="use">NEWS</span>-<span class="web">NEWS</span>.ru
+            </a>
+           </div>
+
+
+           <div class="top-menu">
+             <ul>
+                 <li><a class="clickMenu" href="/all-news">Все новости</a></li>
+                 <li><a href="/all-author">Все авторы</a></li>
+                 <li><a href="/add-news">Добавить новость</a></li>
+             </ul>
+           </div>
+
+
+           <div class="block-top-auth">
+             <p><a href="/login">Вход</a></p>
+             <p><a href="/registration">Регистрация</a></p>
+           </div>
+
+        </header>
+
+        <br>
+        <h1 style="text-align: center;">ALL AUTHOR</h1>
+        <br>
+        <hr>
+        <div>
+        
+        {news_block}
+
+    </body>
+    </html>"""
+
+    return html
 
 
 @app.route('/add-news', methods=['POST', 'GET'])
 def add_news_func():
     if request.method == 'POST':
-        print(request.form.get('author'))  # достаем логин, который ввел пользователь
-        print(request.form.get('content'))  # после достаем пароль
         connection = sqlite3.connect('my_database.db')
         cursor = connection.cursor()
-        cursor.execute('INSERT INTO Users (name, password) VALUES (?, ?)',
-                       (request.form.get('login'), request.form.get('pass')))
+        user = cursor.execute(f"SELECT * FROM Users WHERE name=?", (request.form.get('login'), )).fetchone()
+        connection.commit()
+        cursor.close()
+        connection.close()
+        if user == None:
+            return 'пользователя не существует'
+
+        print(request.form.get('login'))  # достаем логин, который ввел пользователь
+        print(request.form.get('content'))  # после достаем пароль
+
+        connection = sqlite3.connect('my_database.db')
+        cursor = connection.cursor()
+        cursor.execute('INSERT INTO news (name, content) VALUES (?, ?)',
+                       (request.form.get('login'), request.form.get('content')))
+
         connection.commit()
         cursor.close()
         connection.close()
 
-        return 'спасибо за регистрацию'
+        return redirect(f"/all-news/{request.form.get('login')}")
     else:
         return render_template('add_news.html')
+
+
+@app.route('/shop-list')
+def shop_list_func():
+    items = ['товар 1', 'товар 2', 'товар 3']
+    return render_template('shop_list.html', items=items)
 
 
 
